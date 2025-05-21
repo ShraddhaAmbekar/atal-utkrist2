5598a6a7861dfb1d0c62d95d472aabee8b884085
ShraddhaAmbekar <147248222+ShraddhaAmbekar@users.noreply.github.com>
2025-05-21 17:30:08 +0530

    contact image  added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,9 +29,9 @@ const Contact = () => {
         </div>
 
         <div className="container information">
-          <div className="img-con">
-            <img
-              src="https://static.wixstatic.com/media/2e2a49_c8de1e6e955d459aa01defe9f5971dad~mv2.jpg/v1/fill/w_611,h_646,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/2e2a49_c8de1e6e955d459aa01defe9f5971dad~mv2.jpg"
+          <div >
+            <img className="img-con"
+              src="/assets/contact1.jpg"
               alt="school"
             />
           </div>
@@ -49,9 +49,9 @@ const Contact = () => {
             </div>
           </div>
 
-          <div className="img-con order-0 order-md-1">
-            <img
-              src="https://static.wixstatic.com/media/2e2a49_c8de1e6e955d459aa01defe9f5971dad~mv2.jpg/v1/fill/w_611,h_646,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/2e2a49_c8de1e6e955d459aa01defe9f5971dad~mv2.jpg"
+          <div className="order-0 order-md-1">
+            <img className="img-con"
+             src="/assets/contact1.jpg"
               alt="school"
             />
           </div>
@@ -62,13 +62,8 @@ const Contact = () => {
 
               <div className="d-flex justify-content-center align-items-center p-5">
                 <form
-                  className="p-3 rounded shadow"
-                  style={{
-                    width: "100%",
-                    maxWidth: "450px",
-                    backgroundColor: "rgba(255, 255, 255, 0.2)",
-                    backdropFilter: "blur(6px)"
-                  }}
+                  className="p-3 rounded shadow contact-form"
+                
                   onSubmit={handleSubmit}
                 >
                  
@@ -79,16 +74,16 @@ const Contact = () => {
                     </div>
                   )}
 
-                  <div className="mb-2">
+                  <div className="mb-4">
                     <input type="text" className="form-control" placeholder="Your Name" required />
                   </div>
 
-                  <div className="mb-2">
+                  <div className="mb-4">
                     <input type="email" className="form-control" placeholder="Email Address" required />
                   </div>
 
-                  <div className="mb-2">
-                    <textarea className="form-control" rows="2" placeholder="Message" required></textarea>
+                  <div className="mb-4">
+                    <textarea className="form-control" rows="4" placeholder="Message" required></textarea>
                   </div>
 
                   <div className="d-grid">
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <>
       <div className='hero-banner'>
         <div className='welcome'><h2>Welcome to our college</h2></div>
-        <img src="/assets/banner.jpg" alt="hero-image" />
+        <img src="/assets/banner.jpg" alt="banner" />
 
       </div>
       <div className='quote text-center'>
@@ -16,52 +16,47 @@ const Home = () => {
         <span> aliquam. Lorem ipsum dolor sit amet Temporibus, excepturi!</span>
       </div>
 
-      <div className="container information">
-
-        <div>
-          <img className="img-con"
-            src="/assets/Square-02.jpg" alt="square-02"
-          />
-        </div>
-
-        <div className='quote'>
-          <h2>Rural Roots, Global Reach
-
-          </h2><h5 className='quote-text'>Atal Utkrisht Government Inter College, Shrikot (Chinyalisaur, Uttarkashi) is redefining rural education with CBSE-affiliated, English-medium excellence. Our smart classrooms, labs, and library rival private schools, while sports, arts, and digital literacy spark every student’s potential. Inspired by Atal Bihari Vajpayee’s vision, we empower local youth to learn, lead, and build a brighter Himalayan future.
-          </h5>
-        </div>
-
-
-        <div className='order-1 order-md-0'>
-          <div className='quote'>
-            <h2>About Us
-            </h2>
-            <h5 className='quote-text'>Atal Utkrisht Government Inter College, Shrikot—established in 1986 under Uttarakhand’s Education Department—is a proud CBSE-affiliated institution (Affiliation No. 3520105) delivering dual-medium (Hindi & English) excellence. From high-school roots in Hindi, English, Sanskrit, Sciences, Social Sciences, Arts & IT, to our current intermediate offerings in Humanities, Sciences, and Commerce, we’re driven by a legacy of continuous upliftment. As an “Atal Utkrisht” school, we fuse state-of-the-art classrooms, expert faculty, and holistic development to forge leaders of tomorrow.
-
-              At Atal Utkrisht Government Inter College, Shrikot, our strength lies in Expert Faculty—seasoned teachers dedicated to your success. We foster Holistic Growth, blending character, leadership, and ethics into every lesson. Vibrant Campus Life offers sports, cultural clubs, and community service. Through Personalized Learning in small cohorts, every student thrives. And with Future-Ready Skills—critical thinking, digital literacy, and communication—we equip you to excel in tomorrow’s world.
-            </h5>
+    <div className="container information">
+          <div >
+            <img className="img-con"
+              src="/assets/info1.jpg"
+              alt="info1"
+            />
           </div>
-        </div>
-        <div className=" order-0 order-md-1">
-          <img className='img-con'
-            src="/assets/Square-03.jpg"
-            alt="campus"
-          />
-        </div>
+          <div className='quote'>
+            <h2>Rural Roots, Global Reach
 
-<div className="img-con order-md-1">
-            <img
-              src="https://static.wixstatic.com/media/2e2a49_d4fcf8dd8ce44a5ba1e1b9d034d8a43e~mv2.jpg/v1/fill/w_849,h_734,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/2e2a49_d4fcf8dd8ce44a5ba1e1b9d034d8a43e~mv2.jpg"
-              alt="school"
+            </h2><h5 className='quote-text'>Atal Utkrisht Government Inter College, Shrikot (Chinyalisaur, Uttarkashi) is redefining rural education with CBSE-affiliated, English-medium excellence. Our smart classrooms, labs, and library rival private schools, while sports, arts, and digital literacy spark every student’s potential. Inspired by Atal Bihari Vajpayee’s vision, we empower local youth to learn, lead, and build a brighter Himalayan future.
+</h5>
+          </div>
+
+          <div className="order-0 order-md-1">
+            <img className="img-con"
+              src="/assets/info2.jpg" alt='info2'
+            />
+          </div>
+          <div className='order-0 oder-md-1'>   
+            <div className='quote'>
+            <h2>About Us
+            </h2><h5 className='quote-text'>Atal Utkrisht Government Inter College, Shrikot—established in 1986 under Uttarakhand’s Education Department—is a proud CBSE-affiliated institution (Affiliation No. 3520105) delivering dual-medium (Hindi & English) excellence. From high-school roots in Hindi, English, Sanskrit, Sciences, Social Sciences, Arts & IT, to our current intermediate offerings in Humanities, Sciences, and Commerce, we’re driven by a legacy of continuous upliftment. As an “Atal Utkrisht” school, we fuse state-of-the-art classrooms, expert faculty, and holistic development to forge leaders of tomorrow
+
+
+</h5>
+          </div>
+          </div>
+          <div className="order-md-1">
+            <img className="img-con"
+             src="/assets/info3.jpg"
+              alt="info3"
             />
           </div>
           <div className='quote order-md-1'>
-            <h2>
-What sets AUGIC Shrikot apart?
+            <h2>What sets AUGIC Shrikot apart?
 
-            </h2><h5 className='quote-text'>At Atal Utkrisht Government Inter College, Shrikot, our distinction lies in the powerful blend of heart, heritage, and innovation. Our expert educators not only teach—they mentor and nurture each student with care and vision. We offer dynamic learning environments equipped with digital tools, scientific labs, and inclusive practices that support every learner. Rooted in tradition yet embracing technology, we ensure a balanced educational journey. With small cohorts, we cultivate personal attention and big dreams. More than just a school, AUGIC Shrikot is a community built on empathy, action, and purpose. Here, we don’t just educate—we empower, enlighten, and elevate.</h5>
+            </h2><h5 className='quote-text'>At Atal Utkrisht Government Inter College, Shrikot, our distinction lies in the powerful blend of heart, heritage, and innovation. Our expert educators not only teach—they mentor and nurture each student with care and vision. We offer dynamic learning environments equipped with digital tools, scientific labs, and inclusive practices that support every learner. Rooted in tradition yet embracing technology, we ensure a balanced educational journey. With small cohorts, we cultivate personal attention and big dreams. More than just a school, AUGIC Shrikot is a community built on empathy, action, and purpose. Here, we don’t just educate—we empower, enlighten, and elevate</h5>
           </div>
-      </div>
+
+        </div>
       <hr className="custom-hr" />
 
       <div className='container-fluid gallery-container'>
